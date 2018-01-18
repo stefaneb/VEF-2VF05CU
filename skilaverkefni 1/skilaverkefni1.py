46de4f2c60db35cfbd48e2efdb1f83c4c7b0a6d6
Stefán Elís Sigurðsson Beck <stefanbe236@nemi.tskoli.is>
2018-01-18 14:31:39 +0000

    commit 1

---
+++ skilaverkefni 1/skilaverkefni1.py
@@ -0,0 +1,17 @@
+from bottle import route, run
+
+@route('/')
+def index():
+    return "<h2><a style='text-decoration: none;' href='/about'>About</a> <a style='text-decoration: none;' href='/biography'>Biograpy</a> <a style='text-decoration: none;' href='/pictures'>Pictures</a></h2> "
+@route('/about')
+def about():
+    return "<p>This is a website about Steve Jobs.</p>"
+
+@route('/biography')
+def biography():
+    return "<u>Steve Jobs</u> co-founded Apple Computers with Steve Wozniak.<br> Under Jobs' guidance, the company pioneered a series of revolutionary technologies,<br> including the iPhone and iPad."
+
+@route('/pictures')
+def pictures():
+    return "<h2>Hérna væru myndir af Steve Jobs</h2>"
+run()
